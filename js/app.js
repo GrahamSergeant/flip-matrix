767313775067ff85f231e4cdb7e82581ae1e9770
system_user <grahamsergeant4@gmail.com>
2019-04-10 16:06:25 +0100

    added character binary data object for ABC, loaded it into cell matrix with register and kerning, added timer to shift permutations of ABC/BCA/CAB/ABC every 2 seconds to show flip animation on cells, removed ability to flip cells on click

--- js/app.js
+++ js/app.js
@@ -1,26 +1,40 @@
 (function(){
   let model = {
       init : function(){
-        return ({
-          matrixWidth : 5,
-          matrixHeight : 7
+                let spacing = 1;
+                let characterWidth = 4;
+                let characters = 3
+                let lines = 1
+                return ({
+                  matrixWidth : characters * (characterWidth + spacing),
+                  matrixHeight : (7 * lines)
         });
-      }
+      },
+      characterData : characterData()
   };
   
   let controller = {
     init : function(){
       let matrixSize = model.init();
       view.init(matrixSize);
+      view.display("ABC")
+      setTimeout(function(){ view.display("BCA"); }, 2000);
+      setTimeout(function(){ view.display("CAB"); }, 4000);
+      setTimeout(function(){ view.display("ABC"); }, 6000);
+    },
+    getCharacterData : function(character){
+      return model.characterData[character];
     }
   };
   
   let view = {
+    flipCellMatrixArray : [],
     init : function(matrixSize){
+      let cellSize = 20;
+      //build out initial html for a cell matrix with each cell switch off
       let cellMatrix = document.createElement("table");
       document.body.appendChild(cellMatrix);
       cellMatrix.classList.add("cell-matrix");
-      let flipCellMatrixArray = [];
       for (let row = 1; row <= matrixSize.matrixHeight; row++){
         let flipCellRowArray = [];
         let cellRow = document.createElement("tr");
@@ -32,41 +46,57 @@
           let flipCellInner = document.createElement("div");
           flipCell.appendChild(flipCellInner);
           flipCellInner.classList.add("flip-cell-inner");
+          //flipCellInner.classList.add("rotate"); //cell on/off
           let flipCellFront = document.createElement("div");
           flipCellInner.appendChild(flipCellFront);
           flipCellFront.classList.add("flip-cell-front");
           let cellSVG = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-          cellSVG.setAttribute("height", "100%");
-          cellSVG.setAttribute("width", "100%");
+          cellSVG.setAttribute("height", "" + cellSize);
+          cellSVG.setAttribute("width", "" + cellSize);
           flipCellFront.appendChild(cellSVG);
           let svgCircle = document.createElementNS("http://www.w3.org/2000/svg", 'circle');
-          svgCircle.setAttribute("cx", "50");
-          svgCircle.setAttribute("cy", "50");
-          svgCircle.setAttribute("r", "50");
+          svgCircle.setAttribute("cx", "" + (cellSize / 2));
+          svgCircle.setAttribute("cy", "" + (cellSize / 2));
+          svgCircle.setAttribute("r", "" + (cellSize / 2));
           svgCircle.setAttribute("fill", "white");
           cellSVG.appendChild(svgCircle);
           flipCellRowArray.push(flipCellInner);
         }
-        flipCellMatrixArray.push(flipCellRowArray);
+        this.flipCellMatrixArray.push(flipCellRowArray);
       }
-      //
-      flipCellMatrixArray.forEach(function(flipCellRowArray){
-        flipCellRowArray.forEach(function(flipCell){
-          flipCell.onclick = function(){
-                                  console.log('clicked');
-                                  if (!flipCell.classList.contains("rotate")){
-                                    flipCell.classList.add("rotate");
-                                    flipCell.classList.remove("rotate-back");
-                                    console.log("rotate");
-                                  } else {
-                                    flipCell.classList.add("rotate-back");
-                                    flipCell.classList.remove("rotate");
-                                    console.log("rotate back");
-                                  }
-                                };
-        });
-      });
-    
+    },
+    //
+    display : function(message, line){
+      //draw charact starting at register - increment after each character is drawn to add kerning
+      let register = 0;
+      //iterate over each character in passed in message
+      for (let character of message){
+        //get character data stored in an array of row arrays of binary (1 = cell on/ 0 = cell off)
+        let currentCharacter = controller.getCharacterData(character);
+        console.log(character);
+        //iterate over each row
+        for (let row = 0; row < currentCharacter.length; row++){
+          //iterate over each cell in each row
+          for (let cell = 0; cell < currentCharacter[row].length; cell++){
+            //switch cell off if 0 else switch on if 1
+            if (currentCharacter[row][cell] === 0){
+                this.flipCellMatrixArray[row][cell + register].classList.add("rotate");
+                this.flipCellMatrixArray[row][cell + register].classList.remove("rotate-back");
+            } else {
+              this.flipCellMatrixArray[row][cell + register].classList.add("rotate-back");
+              this.flipCellMatrixArray[row][cell + register].classList.remove("rotate");
+            }
+          }
+        }
+        //move register
+        register = register + 4;
+        //add kerning
+        for (let i = 0; i < 7; i++ ){
+          this.flipCellMatrixArray[i][register].classList.add("rotate");
+        }
+        //move register after kerning
+        register++;
+      }
     }
   };
   controller.init();
